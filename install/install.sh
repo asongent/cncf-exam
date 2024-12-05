3beb2d0b92c4dd224f6af5796b2b47b439816743
asongent <asongentle@gmail.com>
2024-12-05 15:32:52 +0000

    installation script added

---
+++ install/install.sh
@@ -0,0 +1,38 @@
+sudo sed -i '/swap/d' /etc/fstab
+sudo swapoff -a
+
+sudo modprobe overlay -
+sudo modprobe br_netfilter 
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF 
+
+
+
+sudo modprobe br_netfilter 
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sudo sysctl --system
+
+sudo mkdir -p /etc/containerd
+sudo apt install curl -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update -y 
+sudo apt install -y containerd.io
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt update -y
+sudo apt -y install kubelet=1.30.5-* kubeadm=1.30.5-* kubectl=1.30.5-*
